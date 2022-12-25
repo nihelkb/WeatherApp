238573341dca9e2d352a1524e6ae7304ce452d22
Julio2666 <julioman266@gmail.com>
2022-12-25 17:07:14 +0100

    hoggg

--- app/src/main/java/com/example/weatherapp/MainActivity.java
+++ app/src/main/java/com/example/weatherapp/MainActivity.java
@@ -7,7 +7,7 @@ import android.os.Bundle;
 public class MainActivity extends AppCompatActivity {
 
     @Override
-    //aaaaaaaaaa
+    //aaaaaaaaaadd
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
